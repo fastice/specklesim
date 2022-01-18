690bd278c8933c7836ab2c7475b23d3fcf367b65
Ian Joughin <ian@apl.washington.edu>
2022-01-18 09:58:35 -0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+__all__ = ['whiteNoisePatch', 'overSamplePatch', 'patchShift', 'nccIntPatches',
+           'nccPatches', 'correlatedShiftedPatches', 'speckleJobs',
+           'osSubPix', 'osSubPixGaussian', 'gaussFit']
+
+from nisarfunc.speckleSim import whiteNoisePatch
+from nisarfunc.speckleSim import overSamplePatch
+from nisarfunc.speckleSim import patchShift
+from nisarfunc.speckleSim import nccIntPatches
+from nisarfunc.speckleSim import nccPatches
+from nisarfunc.speckleSim import correlatedShiftedPatches
+from nisarfunc.speckleSim import speckleJobs
+from nisarfunc.speckleSim import osSubPix
+from nisarfunc.speckleSim import osSubPixGaussian
+from nisarfunc.speckleSim import gaussFit
